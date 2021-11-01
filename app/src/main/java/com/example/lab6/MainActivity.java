0de520150aedd49c83338a22034ddc11fe38db3b
lukas <lwehrli@wisc.edu>
2021-11-01 15:07:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.shape.ShapeAppearanceModel;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private final LatLng mDestinationLatLng = new LatLng(43.075995619695874, -89.40434479621405);
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.addMarker(new MarkerOptions()
+                .position(mDestinationLatLng)
+                .title("Destination"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mDestinationLatLng));
+    }
+}
