d969b679e0bb9346282616ed32cc5a723880ad06
lukas <lwehrli@wisc.edu>
2021-11-04 11:59:19 -0500

    Updated getting location

--- app/src/main/java/com/example/lab6/MainActivity.java
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -16,13 +16,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends FragmentActivity {
 
     private final LatLng mDestinationLatLng = new LatLng(43.075995619695874, -89.40434479621405);
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
-    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,18 +43,19 @@ public class MainActivity extends FragmentActivity {
     private void displayMyLocation(){
         int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION);
-
         if (permission == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }else{
-            mFusedLocationProviderClient.getLastLocation().addOnCompleteListener(this, task -> {
-               Location mLastKnownLocation = task.getResult();
-               if(task.isSuccessful() && mLastKnownLocation != null){
-                   mMap.addPolyline(new PolylineOptions().add(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), mDestinationLatLng));
-                   LatLng newpos = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
-                   mMap.addMarker(new MarkerOptions()
-                           .position(newpos)
-                           .title("Current Location"));
+            mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if(location != null) {
+                        mMap.addPolyline(new PolylineOptions().add(new LatLng(location.getLatitude(), location.getLongitude()), mDestinationLatLng));
+                        LatLng newpos = new LatLng(location.getLatitude(), location.getLongitude());
+                        mMap.addMarker(new MarkerOptions()
+                                .position(newpos)
+                                .title("Current Location"));
+                    }
                }
             });
         }
